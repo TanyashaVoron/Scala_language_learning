c3253758be941cfbeda58e77e740d0551fef1ca2
tatianavoronina <tvm010203@mail.ru>
2023-02-16 12:27:36 +0500

    Воронина Татьяна exercises01

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -1,28 +1,36 @@
 package exercises01
 
 class Vector(val x: Double, val y: Double) {
-  def +(other: Vector): Vector = ???
+  def +(other: Vector): Vector = new Vector(other.x + x, other.y + y)
 
-  def -(other: Vector): Vector = ???
+  def -(other: Vector): Vector = new Vector(x - other.x, y - other.y)
 
-  def *(scalar: Double): Vector = ???
+  def *(scalar: Double): Vector = new Vector(x * scalar, y * scalar)
 
-  def unary_- : Vector = ???
+  def unary_- : Vector = *(-1)
 
-  def euclideanLength: Double = ???
+  def euclideanLength: Double = Math.sqrt(x * x + y * y)
 
-  def normalized: Vector = ???
+  def normalized: Vector =
+    if (x == 0 && y == 0) new Vector(0, 0)
+    else {
+      val euclideanLength_ = euclideanLength
+      new Vector(x / euclideanLength_, y / euclideanLength_)
+    }
 
-  override def equals(other: Any): Boolean = ???
+  override def equals(other: Any): Boolean = other match {
+    case Vector(x, y) => (this.x == x & this.y == y)
+    case _            => false
+  }
 
   // Vector(x, y)
-  override def toString: String = ???
+  override def toString: String = s"Vector($x, $y)"
 }
 
 object Vector {
-  def fromAngle(angle: Double, length: Double): Vector = ???
+  def fromAngle(angle: Double, length: Double): Vector = new Vector(length * Math.cos(angle), length * Math.sin(angle))
 
-  def sum(list: List[Vector]): Vector = ???
+  def sum(list: List[Vector]): Vector = list.foldLeft(new Vector(0, 0))((a, b) => a + b)
 
-  def unapply(arg: Vector): Option[(Double, Double)] = ???
+  def unapply(arg: Vector): Option[(Double, Double)] = Some((arg.x, arg.y))
 }
