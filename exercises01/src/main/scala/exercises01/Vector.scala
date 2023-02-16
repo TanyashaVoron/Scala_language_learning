2f5b509513e4ff5398a879af6f96c4577cbaa0a4
tatianavoronina <tvm010203@mail.ru>
2023-02-16 12:55:11 +0500

    воронина Татьяна exercises01

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -1,29 +1,30 @@
 package exercises01
 
 class Vector(val x: Double, val y: Double) {
-  def +(other: Vector): Vector = new Vector(other.x+x,other.y+y)
+  def +(other: Vector): Vector = new Vector(other.x + x, other.y + y)
 
-  def -(other: Vector): Vector = new Vector(x-other.x,y-other.y)
+  def -(other: Vector): Vector = new Vector(x - other.x, y - other.y)
 
-  def *(scalar: Double): Vector = new Vector(x*scalar,y*scalar)
+  def *(scalar: Double): Vector = new Vector(x * scalar, y * scalar)
 
   def unary_- : Vector = *(-1)
 
-  def euclideanLength: Double = Math.sqrt(x*x+y*y)
+  def euclideanLength: Double = Math.sqrt(x * x + y * y)
 
-  def normalized: Vector = if (x == 0 && y == 0) new Vector(0,0)
-  else new Vector(x/euclideanLength,y/euclideanLength)
+  def normalized: Vector =
+    if (x == 0 && y == 0) new Vector(0, 0)
+    else new Vector(x / euclideanLength, y / euclideanLength)
 
-  override def equals(other: Any): Boolean = other.equals(x,y)
+  override def equals(other: Any): Boolean = other.equals(x, y)
 
   // Vector(x, y)
-  override def toString: String = new String("Vector("+x+", "+y+")")
+  override def toString: String = new String("Vector(" + x + ", " + y + ")")
 }
 
 object Vector {
-  def fromAngle(angle: Double, length: Double): Vector = new Vector(length*Math.cos(angle), length*Math.sin(angle))
+  def fromAngle(angle: Double, length: Double): Vector = new Vector(length * Math.cos(angle), length * Math.sin(angle))
 
-  def sum(list: List[Vector]): Vector = list.foldRight(new Vector(0,0))((a,b)=>a+b)
+  def sum(list: List[Vector]): Vector = list.foldRight(new Vector(0, 0))((a, b) => a + b)
 
-  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x,arg.y)
+  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x, arg.y)
 }
