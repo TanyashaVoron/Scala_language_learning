2b6ac4689c6b438db140b6e28b8184f68049a046
tatianavoronina <tvm010203@mail.ru>
2023-03-02 20:47:09 +0500

    Воронина Татьяна exercises02

--- exercises02/src/main/scala/exercises02/Counter.scala
+++ exercises02/src/main/scala/exercises02/Counter.scala
@@ -6,18 +6,38 @@ object Counter {
     * Посчитать количество вхождений слов в тексте
     * слово отделено символами [\s.,!?:\n\t\r]
     */
-  def countWords(text: String): Map[String, Int] = ???
+  def countWords(text: String): Map[String, Int] =
+    text
+      .split("[\\s.,!?:\\n\\t\\r()]")
+      .filter(_ != "")
+      .map(a => a.toLowerCase)
+      .foldLeft(Map.empty[String, Int].withDefaultValue(0)) {
+        case (count, a) => count.updated(a, count(a) + 1)
+      }
 
   /**
     * Посчитать количество вхождений английских слов в тексте
     * слово отделено символами [\s.,!?:\n\t\r]
     */
-  def countEnglishWords(text: String): Map[String, Int] = ???
+  def countEnglishWords(text: String): Map[String, Int] =
+    text
+      .split("[А-Яа-я\\s.,!?:\\n\\t\\r()]")
+      .filter(_ != "")
+      .map(a => a.toLowerCase)
+      .foldLeft(Map.empty[String, Int].withDefaultValue(0)) {
+        case (count, a) => count.updated(a, count(a) + 1)
+      }
 
   /**
     * Посчитать количество вхождений чисел в тексте
     * число отделено символами [\s!?:\n\t\r]
     */
-  def countNumbers(text: String): Map[String, Int] = ???
-
+  def countNumbers(text: String): Map[String, Int] =
+    text
+      .split("[А-Яа-яA-Za-z\\s!?:\\-\\n\\t\\r()]")
+      .filter(_ != "")
+      .filter(_ != ",")
+      .foldLeft(Map.empty[String, Int].withDefaultValue(0)) {
+        case (count, a) => count.updated(a, count(a) + 1)
+      }
 }
