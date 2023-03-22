636be6f683d9e734227b7786687b5e831d5afccb
tatianavoronina <tvm010203@mail.ru>
2023-03-22 13:20:31 +0500

    Воронина Татьяна exercises04

--- exercises04/src/main/scala/exercises04/calculator/Calculator.scala
+++ exercises04/src/main/scala/exercises04/calculator/Calculator.scala
@@ -14,32 +14,32 @@ class Calculator[T: Integral] {
     case Mul(left, right) =>
       (calculate(left), calculate(right)) match {
         case (Success(v1), Success(v2)) => Success(v1 * v2)
-        case _ => DivisionByZero
+        case _                          => DivisionByZero
       }
 
     case Plus(left, right) =>
       (calculate(left), calculate(right)) match {
         case (Success(v1), Success(v2)) => Success(v1 + v2)
-        case _ => DivisionByZero
+        case _                          => DivisionByZero
       }
 
     case Minus(left, right) =>
       (calculate(left), calculate(right)) match {
         case (Success(v1), Success(v2)) => Success(v1 - v2)
-        case _ => DivisionByZero
+        case _                          => DivisionByZero
       }
 
     case Div(left, right) =>
       (calculate(left), calculate(right)) match {
         case (Success(v1), Success(v2)) if !isZero(v2) => Success(v1 / v2)
-        case _ => DivisionByZero
+        case _                                         => DivisionByZero
       }
 
     case If(iff, cond, left, right) =>
       calculate(cond) match {
         case Success(v) if iff(v) => calculate(left)
-        case Success(_) => calculate(right)
-        case _ => DivisionByZero
+        case Success(_)           => calculate(right)
+        case _                    => DivisionByZero
       }
   }
 }
