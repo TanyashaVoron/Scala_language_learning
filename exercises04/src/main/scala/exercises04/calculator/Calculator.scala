b558b508de921f8596ccfb5ce1d7d19c33d2e61a
tatianavoronina <tvm010203@mail.ru>
2023-03-22 12:14:19 +0500

    Воронина Татьяна exercises04

--- exercises04/src/main/scala/exercises04/calculator/Calculator.scala
+++ exercises04/src/main/scala/exercises04/calculator/Calculator.scala
@@ -7,5 +7,39 @@ class Calculator[T: Integral] {
   def isZero(t: T): Boolean =
     t == implicitly[Integral[T]].zero
 
-  def calculate(expr: Expr[T]): Result[T] = ???
+  def calculate(expr: Expr[T]): Result[T] = expr match {
+
+    case Val(v) => Success(v)
+
+    case Mul(left, right) =>
+      (calculate(left), calculate(right)) match {
+        case (Success(v1), Success(v2)) => Success(v1 * v2)
+        case _ => DivisionByZero
+      }
+
+    case Plus(left, right) =>
+      (calculate(left), calculate(right)) match {
+        case (Success(v1), Success(v2)) => Success(v1 + v2)
+        case _ => DivisionByZero
+      }
+
+    case Minus(left, right) =>
+      (calculate(left), calculate(right)) match {
+        case (Success(v1), Success(v2)) => Success(v1 - v2)
+        case _ => DivisionByZero
+      }
+
+    case Div(left, right) =>
+      (calculate(left), calculate(right)) match {
+        case (Success(v1), Success(v2)) if !isZero(v2) => Success(v1 / v2)
+        case _ => DivisionByZero
+      }
+
+    case If(iff, cond, left, right) =>
+      calculate(cond) match {
+        case Success(v) if iff(v) => calculate(left)
+        case Success(_) => calculate(right)
+        case _ => DivisionByZero
+      }
+  }
 }
