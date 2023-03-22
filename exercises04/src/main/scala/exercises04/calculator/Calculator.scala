af8d6e778a18a285ffe5e885ae96273c1bc9718b
tatianavoronina <tvm010203@mail.ru>
2023-03-22 21:52:07 +0500

    Воронина Татьяна exercises04

--- exercises04/src/main/scala/exercises04/calculator/Calculator.scala
+++ exercises04/src/main/scala/exercises04/calculator/Calculator.scala
@@ -7,33 +7,31 @@ class Calculator[T: Integral] {
   def isZero(t: T): Boolean =
     t == implicitly[Integral[T]].zero
 
+  def caseOperation(left: Expr[T], right: Expr[T], operation: Char): Result[T] = {
+    (calculate(left), calculate(right)) match {
+      case (Success(v1), Success(v2)) =>
+        operation match {
+          case '+'                => Success(v1 + v2)
+          case '-'                => Success(v1 - v2)
+          case '*'                => Success(v1 * v2)
+          case '/' if !isZero(v2) => Success(v1 / v2)
+          case _                  => DivisionByZero
+        }
+      case _ => DivisionByZero
+    }
+  }
+
   def calculate(expr: Expr[T]): Result[T] = expr match {
 
     case Val(v) => Success(v)
 
-    case Mul(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (Success(v1), Success(v2)) => Success(v1 * v2)
-        case _                          => DivisionByZero
-      }
+    case Mul(left, right) => caseOperation(left, right, '*')
 
-    case Plus(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (Success(v1), Success(v2)) => Success(v1 + v2)
-        case _                          => DivisionByZero
-      }
+    case Plus(left, right) => caseOperation(left, right, '+')
 
-    case Minus(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (Success(v1), Success(v2)) => Success(v1 - v2)
-        case _                          => DivisionByZero
-      }
+    case Minus(left, right) => caseOperation(left, right, '-')
 
-    case Div(left, right) =>
-      (calculate(left), calculate(right)) match {
-        case (Success(v1), Success(v2)) if !isZero(v2) => Success(v1 / v2)
-        case _                                         => DivisionByZero
-      }
+    case Div(left, right) => caseOperation(left, right, '/')
 
     case If(iff, cond, left, right) =>
       calculate(cond) match {
