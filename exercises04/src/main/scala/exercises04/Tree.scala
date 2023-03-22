9df4e80ad90f19a601f824aeba142734eb8f99ae
tatianavoronina <tvm010203@mail.ru>
2023-03-22 13:13:43 +0500

    Воронина Татьяна exercises04

--- exercises04/src/main/scala/exercises04/Tree.scala
+++ exercises04/src/main/scala/exercises04/Tree.scala
@@ -1,19 +1,34 @@
 package exercises04
 
 sealed trait Tree[+A]
-final case class Leaf[A](value: A) extends Tree[A]
+final case class Leaf[A](value: A)                        extends Tree[A]
 final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 // Необходимо реализовать операции на бинарном дереве
 object Tree {
-  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = ???
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+    case Leaf(v)             => f(v)
+  }
 
-  def size[A](t: Tree[A]): Int = ???
+  def size[A](t: Tree[A]): Int = t match {
+    case Branch(left, right) => size(left) + size(right) + 1
+    case Leaf(_)             => 1
+  }
 
-  def max(t: Tree[Int]): Int = ???
+  def max(t: Tree[Int]): Int = t match {
+    case Branch(left, right) => max(left) max max(right)
+    case Leaf(v)             => v
+  }
 
-  def depth[A](t: Tree[A]): Int = ???
+  def depth[A](t: Tree[A]): Int = t match {
+    case Branch(left, right) => (depth(left) max depth(right)) + 1
+    case Leaf(_)             => 1
+  }
 
   // тут может пригодиться явное указание типа
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+    case Leaf(v)             => Leaf(f(v))
+  }
 }
