52cde678f13b8f094630c127cf8a588c7ee8715d
tatianavoronina <tvm010203@mail.ru>
2023-03-29 22:11:27 +0500

    Воронина Татьяна exercises05

--- exercises05/src/main/scala/exercises05/parser/Examples.scala
+++ exercises05/src/main/scala/exercises05/parser/Examples.scala
@@ -3,6 +3,8 @@ package exercises05.parser
 import exercises05.either.EitherCombinators._
 import Error._
 
+import scala.util.matching.Regex
+
 object Examples {
 
   /**
@@ -12,8 +14,25 @@ object Examples {
     * если rawUser.id не парсится в Long то функция должна вернуть None
     * если rawUser.banned, то вернуть None
     * используйте for-comprehension
+      if passport.matches("\\d{4} \\d{6}")
     */
-  def transformToOption(rawUser: RawUser): Option[User] = ???
+  private val passportPattern: Regex = "(\\d{4}) (\\d{6})".r
+  private def parsePassport(passport: String): Option[Passport] = {
+    passport match {
+      case passportPattern(s, num) => Some(Passport(s.toLong, num.toLong))
+      case _                       => None
+    }
+  }
+
+  def transformToOption(rawUser: RawUser): Option[User] =
+    for {
+      firstName  <- rawUser.firstName
+      secondName <- rawUser.secondName
+      passport_ = rawUser.passport.flatMap(parsePassport)
+      if rawUser.passport.isEmpty || passport_.isDefined
+      if !rawUser.banned
+      id <- rawUser.id.toLongOption
+    } yield User(id, UserName(firstName, secondName, rawUser.thirdName), passport_)
 
   /**
     * если rawUser.firstName или rawUser.secondName == None, то функция должна вернуть Left(InvalidName)
@@ -29,5 +48,15 @@ object Examples {
     * используйте for-comprehension
     * но для того, чтобы for-comprehension заработал надо реализовать map и flatMap в Either
     */
-  def transformToEither(rawUser: RawUser): Either[Error, User] = ???
+  def transformToEither(rawUser: RawUser): Either[Error, User] = {
+    for {
+      _  <- if (!rawUser.banned) Right(()) else Left(Banned)
+      id <- Either.fromOption(rawUser.id.toLongOption)(InvalidId)
+      passportOpt <- Either.fromOption(
+        if (rawUser.passport.isEmpty) Some(None) else rawUser.passport.flatMap(parsePassport).map(Some(_))
+      )(InvalidPassport)
+      firstName  <- Either.fromOption(rawUser.firstName)(InvalidName)
+      secondName <- Either.fromOption(rawUser.secondName)(InvalidName)
+    } yield User(id, UserName(firstName, secondName, rawUser.thirdName), passportOpt)
+  }
 }
